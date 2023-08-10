ab8bf516621497fa067d38e3d13f65f7501ce586
aydaghasemi66 <aydaghasemi66@gmail.com>
2023-08-10 06:46:56 -0700

    6

--- config/settings.py
+++ config/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 STATIC_ROOT = BASE_DIR/'/static'
-MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_ROOT = BASE_DIR/'/media'
 STATICFILES_DIRS = [
     BASE_DIR/'static',
     BASE_DIR/'media',
--- courses/views.py
+++ courses/views.py
@@ -1,23 +1,26 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import Course
 from courses.models import Category
-# Create your views here.
 
-
-def courses(request ,cat = None,teacher = None):
+def courses(request, cat=None, teacher=None):
     category = Category.objects.all()
+    
     if cat:
-        course = Course.objects.filter(category__name=cat)
+        course_list = Course.objects.filter(category__name=cat)
     elif teacher:
-        course = Course.objects.filter(teacher__info__username = teacher)
+        course_list = Course.objects.filter(teacher__info__username=teacher)
     else:
-        course = Course.objects.filter(status=True)
+        course_list = Course.objects.filter(status=True)
     
-    context ={
-        "courses": course,
-        'category':category,
+    paginator = Paginator(course_list, 2)  
+    page_number = request.GET.get('page')
+    courses = paginator.get_page(page_number)
+    
+    context = {
+        "courses": courses,
+        'category': category,
     }
-    return render(request,'courses/course.html', context=context)
+    return render(request, 'courses/course.html', context=context)
 
-    
                           
