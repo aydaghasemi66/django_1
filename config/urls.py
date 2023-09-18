e5cc10f20736e887b2019a433f1225ea6c6a9066
aydaghasemi66 <aydaghasemi66@gmail.com>
2023-09-18 15:04:19 -0700

    .

--- config/urls.py
+++ config/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("root.urls")),
-    path('account/', include('account.urls', namespace='account')),
+    path('accounts/', include('allauth.urls')),
     path("courses/",include("courses.urls")),
 
 ]
