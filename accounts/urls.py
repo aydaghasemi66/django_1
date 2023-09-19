30888304ef8d9f4c4acb62cb84faede1fd136995
aydaghasemi66 <aydaghasemi66@gmail.com>
2023-09-19 12:34:34 -0700

    ..

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,15 +1,18 @@
-from django.urls import include, path
-from .views import *
-
+from django.urls import path
+from allauth.account.views import LogoutView,SignupView
+from .views import*
+app_name='accounts'
 urlpatterns = [
-
-    path("signup/", CustomSignupView.as_view(), name='account_signup'),
-    path('login/', CustomLoginView.as_view(), name='account_login'),
-    path('change-password/', CustomChangePasswordView.as_view(), name='account_change_password'),
+    path('logout/', LogoutView.as_view(), name='account_logout'),
+    path('signup/',account_signup, name='account_signup'),
+    path('login/',account_login , name='account_login'),
+    path('change_email/', change_email, name='account_email'),
+    path('password_reset/', password_reset, name='password_reset'),
+    path('password_reset/done/',password_reset_done, name='password_reset_done'),
+    path('change-password/',change_password, name='change_password'),
+  
+  
 ]
-
-
-
 
 
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,59 @@
-from allauth.account.views import SignupView, LoginView, PasswordChangeView
+from django.shortcuts import render, redirect
+from allauth.account.forms import SignupForm, LoginForm
+from allauth.account.views import PasswordResetView, PasswordResetDoneView
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
-from .forms import *
+def account_signup(request):
+    if request.user.is_authenticated:
+        return redirect('/')
 
-class CustomSignupView(SignupView):
-    template_name = "account/signup.html" 
-    form_class = CustomSignupForm  
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save(request)
+            messages.success(request, 'Your profile is updated successfully')
+            return redirect('/')
+        else:
+            messages.error(request, 'Invalid')
+    else:
+        form = SignupForm()
 
+    return render(request, 'account/signup.html', {'form': form})
 
+def account_login(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    
+    if request.method == 'POST':
+        form = LoginForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('login')  # Use 'login' field for username
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+    else:
+        form = LoginForm()
 
-class CustomChangePasswordView(PasswordChangeView):
-    template_name = "account/change_password_custom.html" 
-    form_class = CustomChangePasswordForm  
+    return render(request, 'account/login.html', {'form': form})
 
+def account_logout(request):
+    logout(request)
+    return redirect('/')
 
-class CustomLoginView(LoginView):
-    template_name = "account/login_custom.html" 
-    form_class = CustomLoginForm  
+def change_email(request):
+    return render(request, 'account/change_email.html')
+
+def password_reset(request):
+    if request.method == 'POST':
+        messages.success(request, "Password reset email sent successfully.")
+        return PasswordResetView.as_view()(request)
+    return render(request, 'account/password_reset_form.html')
+
+def password_reset_done(request):
+    return render(request, 'account/password_reset_done.html')
+
+def change_password(request):
+    return render(request, 'account/password_change.html')
